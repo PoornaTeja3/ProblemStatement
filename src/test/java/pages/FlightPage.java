ebe3bd42ef9289ac4aa404995a0d763d50707065
Poorna Teja Karepati <poornatejak333@gmail.com>
2026-03-10 11:45:13 +0530

    Added wait for flight selection

--- src/test/java/pages/FlightPage.java
+++ src/test/java/pages/FlightPage.java
@@ -2,23 +2,26 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class FlightPage {
 
     WebDriver driver;
 
     public FlightPage(WebDriver driver) {
-
         this.driver = driver;
-
     }
 
     By chooseFlight = By.xpath("(//input[@value='Choose This Flight'])[1]");
 
     public void selectFlight() {
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(chooseFlight));
+
         driver.findElement(chooseFlight).click();
-
     }
-
 }
